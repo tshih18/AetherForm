8e4bc1ee85d6bf5c9826736d0e02d3f9746a0e74
Theodore Shih <tshih18@yahoo.com>
2017-10-10 17:21:21 -0700

    forgot password email works, need to make page to reset password

--- aether/routes/index.js
+++ aether/routes/index.js
@@ -1,25 +1,32 @@
 // import express to use it
-var express = require('express');
+const express = require('express');
 // execute express
-var router = express.Router();
+const router = express.Router();
 // import fs library
-var fs = require('fs');
+const fs = require('fs');
 // import mongodb
-var mongo = require('mongodb').MongoClient;
+const mongo = require('mongodb').MongoClient;
 // import mongoose
-var mongoose = require('mongoose');
-
+const mongoose = require('mongoose');
 // get object ID
-var objectID = require('mongodb').ObjectID;
+const objectID = require('mongodb').ObjectID;
 // used for testing
-var assert = require('assert');
-
-var bcrypt = require('bcrypt');
-
-var os = require("os");
+const assert = require('assert');
+// for password encryption
+const bcrypt = require('bcrypt');
+// for line breaks
+const os = require("os");
+// to send emails
+const nodemailer = require('nodemailer');
+// gmail authentication
+const xoauth2 = require('xoauth2');
+// to generate random token for password Reset
+const crypto = require('crypto');
+// to avoid nesting callbacks within callbacks within callbacks
+const async = require('async');
 
 // parse response body
-var bodyParser = require('body-parser');
+const bodyParser = require('body-parser');
 // parse application/x-www-form-urlencoded
 router.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
@@ -106,7 +113,7 @@ router.post('/register', function(request, response, next) {
   // check email - if email found - error
   User.findOne({email: email}, function(error, email) {
     if (email) {
-      errorMessage += os.EOL + "Email taken";
+      errorMessage += "Email taken";
       passed = false;
     }
   });
@@ -115,7 +122,16 @@ router.post('/register', function(request, response, next) {
   Key.findOne({key: key}, function(error, keyFound) {
     assert.equal(null, error);
     if (!keyFound) {
-      errorMessage += os.EOL + "Invalid Key";
+      (errorMessage == "") ? errorMessage += "Invalid Key" : errorMessage += ", Invalid Key";
+      passed = false;
+    }
+  });
+
+  // check duplicate key - if key found - error
+  User.findOne({key: key}, function(error, usedKeyFound) {
+    assert.equal(null, error);
+    if (usedKeyFound) {
+      (errorMessage == "") ? errorMessage += "Key taken" : errorMessage += ", Key taken";
       passed = false;
     }
   });
@@ -124,7 +140,7 @@ router.post('/register', function(request, response, next) {
   User.findOne({username: username}, function(error, username) {
     assert.equal(null, error);
     if (username) {
-      errorMessage = "Username taken" + os.EOL + errorMessage;
+      (errorMessage == "") ? errorMessage += "Username taken" : errorMessage = "Username taken, " + errorMessage;
       passed = false;
     }
     if (passed) {
@@ -182,8 +198,91 @@ router.post('/login', function(request, response, next) {
   });
 });
 
+router.post('/forgotPassword', function(request, response) {
 
-router.get('/logout', function(request, response) {
+  var email = request.body.email;
+
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(error, buf) {
+        assert.equal(null, error);
+        var token = buf.toString('hex');
+        done(error, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: email }, function(error, user) {
+        assert.equal(null, error);
+        if (!user) {  // display something
+          return response.render('index', {forgotError: 'Invalid email'});
+        }
+
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+
+        user.save(function(error) {
+          done(error, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      // create reusable transporter object using the default SMTP transport
+      let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+          type: 'OAuth2',
+          user: 'aetherforms@gmail.com',
+          clientId: '887921205506-qnidqf0nf9n587ubkmsrnufq05rn352j.apps.googleusercontent.com',
+          clientSecret: 'ITEzdWwrOK5yBTfOMLYNejB1',
+          refreshToken: '1/C6z1EyaZE9hivbOeHbaTX4_f4jUQUGkbXzP_1Lqy9Lz41UzdiqwDo-iMBoQpOXKJ',
+          accessToken: 'ya29.GlvgBF6A-TA2P5FXonL5kbJjOidpv931tQJ1fLV_fzc7ImZzSheSg5ybwy7-DIMldzEEfE5d14nFUFzKbqzAyRKNGUNZzKcUNanLzlKK0zDVie3vRK5hharrjlgC'
+        }
+      });
+
+      // setup email data with unicode symbols
+      let mailOptions = {
+        from: '"Aether Forms 👻" <aetherforms@gmail.com>',
+        to: email,
+        subject: 'Password Reset',
+        text: 'You are receiving this message because a request has been sent to request password for your account.\n\n' +
+          'Please click on the following link to complete the process:\n\n' +
+          'http://' + request.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore the email and your password will remain unchanged.\n'
+      };
+
+      // send mail with defined transport object
+      transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+          return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+
+        response.render('index', {Success: 'Email has been sent!'});
+      });
+
+    }
+  ],
+  function(error) {
+    if (error) return next(error);
+    response.error('index');
+  });
+});
+
+router.get('/reset/:token', function(request, response) {
+  var token = request.params.token;
+  User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } }, function(error, user) {
+    assert.equal(null, error);
+    if (!user) {
+      console.log('Password reset token is invalid or has expired.');
+      return response.render('index');
+    }
+    console.log(request.user);
+    response.render('index');
+  });
+});
+
+router.post('/logout', function(request, response) {
   request.session.destroy();
   response.redirect('/');
 });
